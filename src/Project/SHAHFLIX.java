a69f6f4ba8d9e2b11ad1e7b32f1d606f88f366c4
League Student <league@iMac.attlocal.net>
2019-08-01 19:56:36 -0700

    Project - Shahflix :poop:

---
+++ src/Project/SHAHFLIX.java
@@ -0,0 +1,45 @@
+package Project;
+import java.awt.Button;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class SHAHFLIX {
+
+void flix() {
+	JLabel Label = new JLabel();
+	JButton Button = loadImage("download.jpg");
+	JFrame Frame = new JFrame();
+	JPanel Panel = new JPanel();
+	
+	Frame.add(Panel);
+	Frame.setVisible(true);
+	Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	Panel.add(Label);
+	Panel.add(Button);
+	Frame.setSize(500, 500);
+	
+}
+public static void main(String[] args) {
+SHAHFLIX shah = new SHAHFLIX();
+
+shah.flix();
+	
+	
+	
+	
+	
+}
+private JButton loadImage(String fileName) {
+	URL imageURL = getClass().getResource(fileName);
+	Icon icon = new ImageIcon(imageURL);
+	return new JButton(icon);
+}
+
+
+}
